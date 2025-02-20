40da41655f45d3d003c9dc6d44af7b0cd948b052
alnaimi-github <alnymybdalwas1@gmail.com>
2025-02-21 02:05:42 +0300

    add user input and investment results components, refactor app module and main bootstrap

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,12 @@
+import { Component, computed, inject, input } from '@angular/core';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+  investmentService = inject(InvestmentService);
+ results = computed(() => this.investmentService.resultsData());
+}
---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, inject, output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from '../investment-input-model';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-user-input',
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+  investmentService = inject(InvestmentService);
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
+ onSubmit() {
+
+      this.investmentService.calculateInvestmentResults({
+        initialInvestment : +this.enteredInitialInvestment(),
+        duration : +this.enteredDuration(),
+        expectedReturn : +this.enteredExpectedReturn(),
+        annualInvestment : +this.enteredAnnualInvestment()
+      });
+
+    this.enteredInitialInvestment.set('0');
+    this.enteredAnnualInvestment.set('0');
+    this.enteredExpectedReturn.set('5');
+    this.enteredDuration.set('10');
+
+ }
+}
